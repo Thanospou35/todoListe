d318c4a5b705ef76c053b26484b6ca165c36bf26
unknown <moha.cmr05@gmail.com>
2025-02-04 15:53:13 +0000

    Ajouter

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,11 @@
+const TodoItem = ({ todo, toggleTodo, deleteTodo }) => {
+    return (
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
+        <button onClick={() => deleteTodo(todo.id)}>X</button>
+      </li>
+    );
+  };
+  
+  export default TodoItem;
+  
